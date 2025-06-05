c08f3f3caf67458c3cc05469e143185925496681
Marko Atanasovski <marko.atanasovski95@gmail.com>
2025-06-05 15:03:52 +0200

    Configure WhiteNoise for static files

--- Restaurant_Project/settings.py
+++ Restaurant_Project/settings.py
@@ -128,9 +128,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR / "static",)
+    BASE_DIR / "static"
 ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 
